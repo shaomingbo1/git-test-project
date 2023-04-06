ad39ea5ed47f287e9993066b00ce036e0d195e30
unknown <15801647838@163.com>
2023-04-06 18:37:50 +0800

    2023/4/6

---
+++ spring5/spring_demo2/src/main/java/com/atguigu/spring5/autowire/Emp.java
@@ -0,0 +1,16 @@
+package com.atguigu.spring5.autowire;
+
+public class Emp {
+    private Dept dept;
+
+    public void setDept(Dept dept) {
+        this.dept = dept;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "dept=" + dept +
+                '}';
+    }
+}
